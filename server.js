150917d285775fce776f0018f741debed8b6f101
jkguiang <jkguiang@gmail.com>
2018-11-11 16:19:23 +0000

    renamed post endpoint

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const exec = require('child_process').exec;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
+app.post('/api/integrate', (req, res) => {
   console.log(req.body);
   exec(`maxima -very-quiet -r 'float(integrate(${req.body.integrate},x,${req.body.from},${req.body.to}));'`, function callback(error, stdout, stderr){
     res.send(
