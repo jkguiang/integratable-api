da9340a69d34928c9e77e3c87e7a8b2c6f5bdd3d
jkguiang <jkguiang@gmail.com>
2018-11-11 00:29:13 +0000

    added maxima query

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const exec = require('child_process').exec;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,9 +11,11 @@ app.get('/api/hello', (req, res) => {
 });
 app.post('/api/world', (req, res) => {
   console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+  exec('maxima -r "float(integrate(x^2,x,1,2));"', function callback(error, stdout, stderr){
+    res.send(
+      `I received your POST request. This is what you sent me: ${stdout}`,
+    );
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
